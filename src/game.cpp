8b9f6beb1a3340c98d96e02f63ac3748865f4e82
Qluges <esrandiny@gmail.com>
2026-03-01 10:24:29 +0700

    i added slightly more not done yet game.cpp

--- src/game.cpp
+++ src/game.cpp
@@ -6,13 +6,13 @@
 #include <utility>
 
 #define nl '\n'
-#define REF_MAP_HEIGHT 51
+#define REF_MAP_HEIGHT 51 // 0->20 nothing, 70->90 the core
 #define REF_MAP_WIDTH 50
-#define ACTUAL_TILE_PIXELS 128
 
 // Game file assets begin here
 #define RESOURCES_FILE_PATH "res/res.png"
 #define BGMUSIC_FILE_PATH "res/music.wav"
+//name, coord y-x, width height
 std::unordered_map<std::string, std::pair<std::pair<int, int>, std::pair<int, int>>> resources = {
 	{"player_walk_1", {{192, 210}, {16, 16}}},
 	{"player_walk_2", {{211, 210}, {16, 16}}},
@@ -22,11 +22,39 @@ std::unordered_map<std::string, std::pair<std::pair<int, int>, std::pair<int, in
 	{"player_walk_6", {{287, 210}, {16, 16}}},
 	{"player_walk_7", {{306, 210}, {16, 16}}},
 	{"player_walk_7", {{306, 210}, {16, 16}}},
+	{"player_idle1", {{125,195}, {16,16}}},
+	{"player_idle2", {{441,221}, {16,16}}},
 	{"hook", {{239, 118}, {8, 7}}},
 	{"wall1", {{113, 362}, {16, 16}}},
 	{"wall2", {{134, 349}, {16, 16}}},
 	{"wall3", {{134, 371}, {16, 16}}},
 	{"surface", {{70, 453}, {16, 16}}},
+	{"core_earth", {{80,343}, {16,16}}},
+	{"lava", {{81,369}, {16,16}}},
+	{"spike", {66,378}, {9,7}}},
+	{"hook", {{237,117}, {8,7}}},
+	{"gem_on_profile", {246,160}, {14,9}}},
+	{"playerhealth_bar_1full",{202,161}, {36,10}}},
+	{"playerhealth_bar_2", {202,149},{36,10}}},
+	{"playerhealth_bar_3",{202,138},{36,10}}},
+	{"playerhealth_bar_4",{203,173},{36,10}}},
+	{"playerhealth_bar_5",{203,185},{36,10}}},
+	{"playerhealth_bar_6empty",{203,195},{36,10}}},
+	{"timer_1full", {15,306},{45,17}}},
+	{"timer_2",{15,325},{45,17}}},
+	{"timer_3",{15,346},{45,15}}},
+	{"timer_4",{15,365},{45,15}}},
+	{"timer_5",{14,386},{45,14}}},
+	{"timer_6empty", {14,410}, {45,9}}},
+	{"spider_enemy_idle1", {251,359},{16,16}}},
+	{"spider_enemy_idle2",{270,359},{16,16}}},
+	{"mouse_enemy_walk1",{474,277},{17,12}}},
+	{"mouse_enemy_walk2",{475,322},{18,11}}},
+	{"mouse_enemy_roll",{439,358},{15,15}}},
+	{"play_button_aka_dig", {31,23},{74,25}}},
+	{"leaderboard_button",{31,52}, {74,25}}},
+	
+
 };
 
 Texture2D assets;
@@ -39,7 +67,6 @@ enum RefMapAttr {
 	TUNNEL = 3,
 	SURFACE = 4,
 	CORE = 5,
-	BUCKET = 6
 };
 
 class RefMap {
@@ -51,7 +78,6 @@ class Player {
 private:
 public:
 	Vector2 position;
-	Vector2 position_refmap;
 	Player(Vector2 position) {
 		this->position = position;
 	};
@@ -77,37 +103,33 @@ void map_divide_recurse(std::vector<int>& rows, int start, int end) {
 
 RefMap generate_map() {
 	RefMap refmap;
-
 	for (int i=0; i<REF_MAP_HEIGHT; i++) {
 		for (int j=0; j<REF_MAP_WIDTH; j++) {
 			refmap.data[i][j] = BLANK_SPACE;
 		}
 	}
 
-
 	// draw the platforms
 	std::vector<int> rows_marked;
-	map_divide_recurse(rows_marked, 3, REF_MAP_HEIGHT - 3);
+	map_divide_recurse(rows_marked, 2, REF_MAP_HEIGHT - 3);
 
 	// add mandatory ones
 	for (int a=0; a<REF_MAP_WIDTH; a++) {
-		refmap.data[1][a] = SURFACE;
+		refmap.data[0][a] = SURFACE;
 	}
-	rows_marked.push_back(1);
+	rows_marked.push_back(0);
 
 	for (int a=0; a<REF_MAP_WIDTH; a++) {
 		refmap.data[REF_MAP_HEIGHT - 1][a] = CORE;
 	}
 	rows_marked.push_back(REF_MAP_HEIGHT - 1);
 
-	refmap.data[0][0] = BUCKET;
-
 	// sort everything
 	std::sort(rows_marked.begin(), rows_marked.end());
 
 	// draw the connectors
 	for (int i = 0; i < int(rows_marked.size()); i++) {
-		if (refmap.data[rows_marked[i]][0] != CORE && refmap.data[rows_marked[i]][0] != SURFACE) {
+		if (refmap.data[rows_marked[i]][0] != CORE && refmap.data[i][0] != SURFACE) {
 			for (int a=0; a<REF_MAP_WIDTH; a++) {
 				refmap.data[rows_marked[i]][a] = PLATFORM;
 			}
@@ -117,13 +139,14 @@ RefMap generate_map() {
 		// but probability of getting the same spot just 10 times in a row is basically 3/50^10 so like- idk how unlucky you have to be to get that
 		// sorry why am i so paranoid ..
 		if (i==rows_marked.size()-1) continue;
-		for (int c=0; c<GetRandomValue(2, 9); c++) {
+		for (int c=0; c<GetRandomValue(1, 5); c++) {
 			int random_hit = GetRandomValue(0, REF_MAP_WIDTH-1);
 			// tho if the REF_MAP_RADIUS is super small u might loop a while here
 			while (refmap.data[rows_marked[i]+1][random_hit] == TUNNEL || refmap.data[rows_marked[i]+1][random_hit-1] == TUNNEL || refmap.data[rows_marked[i]+1][random_hit+1] == TUNNEL) {
 				random_hit = GetRandomValue(0, REF_MAP_WIDTH-1);
 			}
 			for (int j=rows_marked[i]+1; j < rows_marked[i+1]; j++) {
+				dbg(j, random_hit, rows_marked);
 				refmap.data[j][random_hit] = TUNNEL;
 			}
 		}
@@ -133,7 +156,7 @@ RefMap generate_map() {
 }
 
 // Sprite or other globals begin here
-Player player(raylib::Vector2(ACTUAL_TILE_PIXELS, SCREEN_HEIGHT/2.0f));
+Player player(raylib::Vector2(0.0, 0.0));
 raylib::Camera2D camera(raylib::Vector2(SCREEN_WIDTH/2.0f, SCREEN_HEIGHT/2.0f), player.position);
 
 // Game functions begin here
@@ -152,7 +175,6 @@ void game_init() {
 				case PLATFORM:    c = '-'; break;
 				case TUNNEL:      c = '|'; break;
 				case SURFACE:     c = '"'; break;
-				case BUCKET:      c = 'v'; break;
 				case CORE:        c = '*'; break;
 				default:          c = '?'; break;
 			}
@@ -176,25 +198,12 @@ void game_draw() {
 
 	BeginMode2D(camera);
 
-	DrawRectangle(0, 0, 100, 100, BLUE);
-
-
 	EndMode2D();
 
 	EndDrawing();
 }
 
 void game_update() {
-	camera.target = raylib::Vector2(std::clamp(player.position.x + 8, float(SCREEN_WIDTH/2.0), float(ACTUAL_TILE_PIXELS * REF_MAP_WIDTH - SCREEN_WIDTH/2.0)), std::clamp(player.position.y + 8, float(SCREEN_WIDTH/2.0), float(ACTUAL_TILE_PIXELS * REF_MAP_WIDTH - SCREEN_HEIGHT/2.0)));
-
-	if (IsKeyDown(KEY_RIGHT)) {
-		if (player.position.x <= ACTUAL_TILE_PIXELS * REF_MAP_WIDTH) player.position.x += 10.0;
-	}
-	if (IsKeyDown(KEY_LEFT)) {
-		 player.position.x -= 10.0;
-	}
-
-	player.position = raylib::Vector2(std::clamp(player.position.x, float(0.0), float(ACTUAL_TILE_PIXELS*REF_MAP_WIDTH)), std::clamp(player.position.y, float(0.0), float(ACTUAL_TILE_PIXELS*REF_MAP_WIDTH)));
-
+	camera.target = player.position;
 
 }
